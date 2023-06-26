c6243272c95ff8897f5b69d08ee56bda4e1438b1
Esther Uduma <estheruduma23@gmail.com>
2023-06-26 04:22:49 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,16 +7,19 @@
  */
 void puts_half(char *str)
 {
-	int len = 0;
+	int a, b, len;
 
-	int half_len;
+	len = 0;
 
-	while (str[len] != '\0')
-	{
+	for (a = 0; str[a] != '\0'; a++)
 		len++;
-	}
-	half_len = (len - 1) / 2;
-	for (int i = half_len + 1; i < len; i++)
-		_putchar(str[i]);
+
+	b = (len / 2);
+
+	if ((len % 2) == 1)
+		b = ((len + 1) / 2);
+
+	for (a = b; str[a] != '\0'; a++)
+		_putchar(str[a]);
 	_putchar('\n');
 }
