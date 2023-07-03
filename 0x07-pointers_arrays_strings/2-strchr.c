eda30837df05c46deae4da2ffb9abfc666e1ae1b
Esther Uduma <estheruduma23@gmail.com>
2023-07-03 07:58:01 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,11 +7,13 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	for (; *s != '\0'; s++)
 	{
 		if (*s == c)
-			return (s;
-		s++;
+			return (s);
+		else if (*(s + 1) == c)
+			return (s + 1);
 	}
-	return (0);
+
+	return (NULL);
 }
