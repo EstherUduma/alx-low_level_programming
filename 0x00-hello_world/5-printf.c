d80145475ee8c7e40326b9dcff987a19e61b426b
Esther Uduma <estheruduma23@gmail.com>
2023-07-31 05:37:10 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,18 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
-* main - Entry point
+* main - This function prints a text to the console using printf
 *
-* Return: Always 0 (Success)
+* Return: Always 0 if successful
+*
+* File_name: 5-printf.c
+*
+* Author: Esther Ann Uduma
 */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
 }
