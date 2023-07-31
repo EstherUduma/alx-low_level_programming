b6d8d53e6fe1744b8ec79a7fe2c3e943eb930e10
Esther Uduma <estheruduma23@gmail.com>
2023-07-31 06:33:26 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
-#include <unistd.h>
 
 /**
-* main - Entry point
+* main - This function prints a line of text to the console
 *
-* Return: Always 1 (Success)
+* Return: Always 0 if successful
+*
+* File_name: 101-quote.c
+*
+* Author: Esther Ann Uduma
 */
+
 int main(void)
 {
-	write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015\-10\-19";
+
+	write(2, text, 59);
+
 	return (1);
 }
