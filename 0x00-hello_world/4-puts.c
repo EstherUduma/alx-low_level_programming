14ee86b5a607befd2a9cb453da4cb44a37d5a741
Esther Uduma <estheruduma23@gmail.com>
2023-07-31 05:19:29 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - This function prints a string of char
+*
+* Return: Always 0 if successful
+*
+* File_name: 4-puts.c
+*
+* Author: Esther Ann Uduma
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
