5bb91fdc47762303302c7d023b20f3db014835f8
Esther Uduma <estheruduma23@gmail.com>
2023-07-31 07:34:54 -0700

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,29 +1,30 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 
 /**
- * main - Checks if a number is positive or negative.
- *
- * Return: Always 0 (Success)
- */
+* main - This function checks if a number is positive or negative
+*
+* Return: Always 0 if successful
+*
+* File_name: 0-positive_or_negative.c
+*
+* Author: Esther Ann Uduma
+*/
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
-	{
 		printf("%d is positive\n", n);
-	}
-	else if (n == 0)
-	{
+	if (n == 0)
 		printf("%d is zero\n", n);
-	}
-	else
-	{
+	if (n < 0)
 		printf("%d is negative\n", n);
-	}
+
 	return (0);
 }
