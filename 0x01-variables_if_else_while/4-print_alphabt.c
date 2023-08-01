353b08a936da265b470477928434a8c2d1ab083d
Esther Uduma <estheruduma23@gmail.com>
2023-08-01 02:43:44 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,23 +1,34 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 /**
- * main - Prints all alphabets without q and e.
- *
- * Return: Always 0 (Success)
- */
+* main - Prints alphabets in lowercase except q and e, followed by a new line
+*
+* Return: Always 0 if successful
+*
+* c1: alphabet a - d
+*
+* c2: alphabet f - p
+*
+* c3: alphabet r - z
+*
+* File_name: 4-print_alphabt.c
+*
+* Author: Esther Ann Uduma
+*/
+
 int main(void)
 {
-	int i;
+	char c1;
+	char c2;
+	char c3;
 
-	for (i = 97; i < 123; i++)
-	{
-		if (i != 101 && i != 113)
-		{
-			putchar(i);
-		}
-	}
+	for (c1 = 'a'; c1 <= 'd'; c1++)
+		putchar(c1);
+	for (c2 = 'f'; c2 <= 'p'; c2++)
+		putchar(c2);
+	for (c3 = 'r'; c3 <= 'z'; c3++)
+		putchar(c3);
 	putchar('\n');
+
 	return (0);
 }
