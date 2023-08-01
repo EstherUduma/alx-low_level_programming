47f999e0f1c027e8f9e125143ab789a2387725c5
Esther Uduma <estheruduma23@gmail.com>
2023-08-01 02:27:34 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 
 /**
- * main - Prints uppercase and lowercase alphabet.
- *
- * Return: Always 0 (Success)
- */
+* main - Prints alphabets in lowercase and uppercase followed by a new line.
+*
+* Return: Always 0 if successful
+*
+* lc: Lowercase
+*
+* uc: Uppercase
+*
+* File_name: 3-print_alphabets.c
+*
+* Author: Esther Ann Uduma
+*/
+
 int main(void)
 {
-char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	int i;
+	char lc;
+	char uc;
 
-	for (i = 0; i < 52; i++)
-	{
-		putchar(alp[i]);
-	}
+	for (lc = 'a'; lc <= 'z'; lc++)
+		putchar(lc);
+	for (uc = 'A'; uc <= 'Z'; uc++)
+		putchar(uc);
 	putchar('\n');
+
 	return (0);
 }
