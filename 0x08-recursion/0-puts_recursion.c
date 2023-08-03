6f9b0281051bae1072ef92b4206ecf50651d4f3f
Esther Uduma <estheruduma23@gmail.com>
2023-08-02 20:44:56 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _puts_recursion: this function prints a string followed by a newline
+*
+* @s: the pointer to the string to be printed
+*
+* Return: void
+*
+* File_name: 0-puts_recursion.c
+*
+* Author: Esther Ann Uduma
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s != '0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
