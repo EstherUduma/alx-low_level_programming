377e51c5f5fe6dd6d996a21b3ebf4cdd76bed3fe
Esther Uduma <estheruduma23@gmail.com>
2023-08-04 03:47:00 -0700

    update

---
+++ 0x09-static_libraries/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _strncat - this function concatinates string
+*
+* @dest: destination string
+*
+* @src: source string
+*
+* @n: no. of bytes to concatenate
+*
+* Return: Always 1 if successful, and 0 if not
+*
+* File_name: 1-strncat.c
+*
+* Author: Esther Ann Uduma
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+}
