c8c496aed9ea379b8fc692f0549b53f1c1dde9a8
Esther Uduma <estheruduma23@gmail.com>
2023-08-04 04:29:40 -0700

    update

---
+++ 0x09-static_libraries/2-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* mul - this function multiplies 2 numbers
+*
+* @a: first integer
+*
+* @b: second integer
+*
+* Return: result
+*
+* File_name: 2-mul.c
+*
+* Author: Esther Ann Uduma
+*/
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
