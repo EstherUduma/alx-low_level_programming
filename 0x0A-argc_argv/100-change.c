fa46fd4fbbb98523964499e1cb1668d25841f4d8
Esther Uduma <estheruduma23@gmail.com>
2023-08-04 09:26:40 -0700

    update

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -5,7 +5,7 @@
 * main - this function prints minimum number coins to change
 *
 * @argc: number of input array
-* 
+*
 * @argv: string inputed
 *
 * Return: 0 if sucessful
@@ -17,33 +17,33 @@
 
 int main(int argc, char *argv[])
 {
-	int coins = 0, cents;
+	int num, j, result;
+	int coins[] = {25, 10, 5, 2, 1};
 
-	if (argc < 2 || argc > 2)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else if (argc == 2)
+
+	num = atoi(argv[1]);
+	result = 0;
+
+	if (num < 0)
 	{
-		if (atoi(argv[1]) <= 0)
+		printf("0\n");
+		return (0);
+	}
+
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= coins[j])
 		{
-			printf("0\n");
-		}
-		else if (atoi(argv[1]) > 0)
-		{
-			cents = atoi(argv[1]);
-			coins = cents / 25;
-			cents = cents % 25;
-			coins = coins + cents / 10;
-			cents = cents % 10;
-			coins = coins + cents / 5;
-			cents = cents % 5;
-			coins = coins + cents / 2;
-			cents = cents % 2;
-			coins = coins + cents / 1;
-			printf("%d\n", coins);
+			result++;
+			num -= coins[j];
 		}
 	}
+
+	printf("%d\n", result);
 	return (0);
 }
