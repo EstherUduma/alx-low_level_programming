6df35942246652440ca5a3423b34c99adeb8a107
Esther Uduma <estheruduma23@gmail.com>
2023-08-08 06:15:21 -0700

    update

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* _strdup - this function returns a pointer to a new string which is a
+* duplicate of the string
+*
+* @str: string
+*
+* Return: NULL, if str == NULL && p == NULL
+*
+* File_name: 1-strdup.c
+*
+* Author: Esther Ann Uduma
+*/
+
+char *_strdup(char *str)
+{
+	int i;
+
+	int j;
+
+	char *p;
+
+	if (str == NULL)
+		return (0);
+
+	while (str[i] != '\0')
+		i++;
+
+	p = malloc((i + 1) * sizeof(char));
+	if (p == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		for (j = 0; j <= i; j++)
+		{
+			*(p + j) = str[j];
+		}
+		p[j] = '\0';
+	}
+
+	return (p);
+}
