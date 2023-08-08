d0c935aa3211c8a672e2434d4a64713102ee22a5
Esther Uduma <estheruduma23@gmail.com>
2023-08-08 08:42:19 -0700

    update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,28 +20,30 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, j, k, l;
 	char *p;
 
-	if (s1 == NULL)
-		s1 = " ";
-	if (s2 == NULL)
-		s2 = " ";
+	if (s1 == NULL || s2 == NULL)
+	{
+		s1 = "";
+		s2 = "";
+	}
 
 	for (i = 0; s1[i] != '\0'; i++)
 		;
 	for (j = 0; s2[j] != '\0'; j++)
 		;
 
-	p = (char *)malloc((i + j + 1) * sizeof(char));
+	p = malloc(sizeof(char) * (i + j + 1));
 
 	if (p == NULL)
 	{
 		free(p);
 		return (NULL);
 	}
+
 	for (k = 0; k < i; k++)
 		p[k] = s1[k];
 
 	l = j;
-	for (j = 0; j <= l; j++)
+	for (j = 0; j <= l; k++, j++)
 		p[k] = s2[j];
 
 	return (p);
