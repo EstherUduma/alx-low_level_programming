5e3e755e4821c60e785ceac5cecf8f372cda0a5a
Esther Uduma <estheruduma23@gmail.com>
2023-08-14 09:25:23 -0700

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -21,25 +21,69 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
+	int len_owner, len_owner;
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
+		return (NULL);
+
+	len_name = _strlen(name);
+	new_dog->name = malloc(sizeof(char) * len_name + 1);
+	if (new_dog->name == NULL)
 	{
+		free(new_dog);
 		return (NULL);
 	}
-
-	new_dog->name = strdup(name);
-	new_dog->age = age;
-	new_dog->owner = strdup(owner);
-
-	if (new_dog->name == NULL || new_dog->owner == NULL)
+	new_dog->name = _strcpy(new_dog->, name);
+	len_owner = _strlen(owner);
+	new_dog->owner = malloc(sizeof(char) * len_owner + 1);
+	if (new_dog->owner == NULL)
 	{
 		free(new_dog->name);
-		free(new_dog->owner);
 		free(new_dog);
-
 		return (NULL);
 	}
 
+	new_dog->owner = _strcpy(new_dog->owner, owner);
+	new_dog->age = age;
+
 	return (new_dog);
+}
+
+/**
+* _strlen - lenght of string
+*
+* @s: pointer
+*
+* Return: length
+*/
+
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+		;
+	return (a);
+}
+
+/**
+* _strcpy - copies a pointed string
+* @dest: destination string
+* @src: source string
+* Return: the address of the destination string
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	int a = 0;
+
+	while (src[a] != '\0')
+	{
+		dest[a] = src[a];
+		a++;
+	}
+
+	dest[a] = '\0';
+	return (dest);
 }
