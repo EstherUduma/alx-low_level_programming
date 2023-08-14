d901159361057c9a8e00cebcedf0d8328fccd4f8
Esther Uduma <estheruduma23@gmail.com>
2023-08-14 07:50:15 -0700

    update

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+
+/**
+* init_dog - function that initialize a variable of type struct dog
+*
+* @d: pointer
+*
+* @name: dog's name
+*
+* @age: dog's age
+*
+* @owner: dog's owner
+*
+* Return: void
+*
+* File_name: 1-init_dog.c
+*
+* Author: Esther Ann Uduma
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
