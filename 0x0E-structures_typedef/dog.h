7dab44e8c938894518a90bee9afd7059a70b7055
Esther Uduma <estheruduma23@gmail.com>
2023-08-14 07:21:19 -0700

    update

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,30 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - dog struct
+*
+* @name: dog's name
+*
+* @age: dog's age
+*
+* @owner: dog's owner
+*
+* File_name: dog.h
+*
+* Author: Esther Ann Uduma
+*/
+
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+typedef struct dog dog_t;
+
+#endif
