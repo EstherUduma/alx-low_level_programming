d570dfe71f8548074fd944bc6a93c13664dce80f
Esther Uduma <estheruduma23@gmail.com>
2023-08-14 06:18:24 -0700

    update

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: 0
+*
+* File_name: 2-main.c
+*
+* Author: Esther Ann Uduma
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
