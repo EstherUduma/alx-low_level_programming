cf5aa82cd69fb641ba5ea77b17c1857677ca1909
Esther Uduma <estheruduma23@gmail.com>
2023-08-16 07:44:41 -0700

    update

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,25 @@
+#include "3-calc.h"
+
+/**
+* op_add - this function adds two integers
+*
+* @a: first integer
+*
+* @b: second integer
+*
+* Return: if successful 0, otherwise 1.
+*
+* File_name: 3-op_functions.c
+*
+* Author: Esther Ann Uduma
+*/
+
+op_add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	printf("%d\n", c);
+
+	return (0);
+}
