8c8bc00fad38871fa23d64961c8cb8be6eaaa084
Esther Uduma <estheruduma23@gmail.com>
2023-08-16 05:29:01 -0700

    update

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+* print_name - function that prints a name
+*
+* @name: the name to be printed
+*
+* @f: function pointer
+*
+* Return: Nothing
+*
+* File_name: 0-print_name.c
+*
+* Author: Esther Ann Uduma
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
