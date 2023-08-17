693d304551aa0b3d8b7be7c57dd2e79a4ef0816f
Esther Uduma <estheruduma23@gmail.com>
2023-08-17 13:56:22 -0700

    update

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,57 @@
+#include "variadic_functions.h"
+
+/**
+* print_all - function that prints anything
+*
+* @format: list of arguments to be passed to the function
+*
+* Return: Nothing
+*
+* File_name: 3-print_all.c
+*
+* Author: Esther Ann Uduma
+*/
+
+void print_all(const char * const format, ...)
+{
+	va_list valist;
+	unsigned int i = 0, j, c = 0;
+	char *str;
+	const char t_arg[] = "cifs";
+
+	va_start(valist, format);
+	while (format[i] && format)
+	{
+		j = 0;
+		while (t_arg[j])
+		{
+			if (c && format[i] == t_arg[j])
+			{
+				printf(", ");
+				break;
+			} j++;
+		}
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(valist, int)), c = 1;
+				break;
+			case 'i':
+				printf("%d", va_arg(valist, int)), c = 1;
+				break;
+			case 'f':
+				printf("%f", va_arg(valist, double)), c = 1;
+				break;
+			case 's':
+				str = va_arg(valist, char *), c = 1;
+				if (!str)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", str);
+				break;
+		} i++;
+	}
+	printf("\n"), va_end(valist);
+}
