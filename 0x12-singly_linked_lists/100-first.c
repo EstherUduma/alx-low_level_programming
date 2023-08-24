3bf3258c955003a7ec5706c87a89ba65d8d22547
Esther Uduma <estheruduma23@gmail.com>
2023-08-24 15:51:28 -0700

    100-first.c update

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+* tortoise - prints a string before executing main
+*
+* Return: nothing
+*
+* File_name: 100-first.c
+*
+* Author: Esther Ann Uduma
+*/
+
+void tortoise(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
