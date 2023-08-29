108b5888cd1f02a77b0e707fd270fe51db8a670e
Esther Uduma <estheruduma23@gmail.com>
2023-08-29 10:28:11 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+* listint_len - returns the number of elements in a linked listint_t list.
+*
+* @h: pointer
+*
+* Return: node
+*
+* File_name: 1-listint_len.c
+*
+* Author: Esther Ann Uduma
+*/
+
+size_t listint_len(const listint_t *h)
+{
+	size_t node = 0;
+
+	while (h)
+	{
+		node++;
+		h = h->next;
+	}
+
+	return (node);
+}
