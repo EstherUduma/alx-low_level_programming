82d79f4735c2d9caaf72b00e543b3d43ec69c79a
Esther Uduma <estheruduma23@gmail.com>
2023-08-29 10:21:36 -0700

    update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,22 +1,27 @@
 #include "lists.h"
 
 /**
- * print_listint - prints elements of list
- * @h: pointer to head of list.
- *
- * Return: n the number of elements in list
- */
+* print_listint - prints all the elements of a listint_t list
+*
+* @h: pointer
+*
+* Return: node
+*
+* File_name: 0-print_listint.c
+*
+* Author: Esther Ann Uduma
+*/
 
 size_t print_listint(const listint_t *h)
 {
-	int n = 0;
+	size_t node = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		n++;
+		node++;
 		h = h->next;
 	}
 
-	return (n);
+	return (node);
 }
