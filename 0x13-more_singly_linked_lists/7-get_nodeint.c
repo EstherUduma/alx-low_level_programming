cd2bb27252144e30ca21b8f3399cd128675b1f54
Esther Uduma <estheruduma23@gmail.com>
2023-08-29 20:18:05 -0700

    update

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+* get_nodeint_at_index - function that gets the nth node.
+*
+* @head: head pointer
+*
+* @index: nth node
+*
+* Return: nth node
+*
+* File_name: 5-free_listint2.c
+*
+* Author: Esther Ann Uduma
+*/
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *p = head, *c;
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+
+	for (i = 0; i < index; i++)
+	{
+		if (p->next == NULL)
+			return (NUL);
+		c = p->next;
+		p = c;
+	}
+	return (p);
+}
