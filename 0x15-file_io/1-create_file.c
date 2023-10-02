1cae3f5f33f5387231f8a057ef50c425dda5b80e
EstherUduma <estheruduma23@gmail.com>
2023-10-02 09:52:59 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+* create_file - function that creates a file
+* @filename: pointer to the filename
+* @text_content: pointer to the text content to be written
+* Return: 1 on success, -1 on failure
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t written_chars;
+	int count = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	written_chars = write(fd, text_content, count);
+
+	if (written_chars == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+
+	return (1);
+}
